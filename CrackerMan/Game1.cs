0267599710e76a73da97052fcd4b4af2ba976f14
Martin Muzikář <muzikarpc@gmail.com>
2017-04-14 11:30:01 +0200

    Project setup

---
+++ CrackerMan/Game1.cs
@@ -0,0 +1,17 @@
+﻿using Nez.Samples.Scenes;
+
+namespace Nez.Samples
+{
+	public class Game1 : Core
+	{
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+            
+			Window.AllowUserResizing = true;
+			scene = new TestScene();
+		}
+	}
+}
+
---
+++ CrackerMan/Scenes/TestScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nez.Samples.Scenes
+{
+    public class TestScene: Scene
+    {
+        public TestScene()
+        {
+            addRenderer(new DefaultRenderer());
+        }
+    }
+}
